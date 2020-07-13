3f45308dbfe112d87b4e707dba5df3c4200c92d5
Ali Parvizi <snparvizi75@gmail.com>
2020-07-13 21:19:31 +0430

    show data

--- partC.py
+++ partC.py
@@ -1,3 +1,21 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+# the two different classes in data
+A = "Iris-setosa"
+B = "Iris-virginica"
+
+color = {
+    A: 'r',
+    B: 'b'
+}
+
+value = {
+    A: 1,
+    B: 0
+}
+
+
 def load(file):
     handle = open(file)
 
@@ -5,20 +23,31 @@ def load(file):
 
     data = data.split('\n')
 
-    x = []
-    y = []
+    _x = []
+    _y = []
     _class = []
 
     for line in data:
-        f1, f2, cls = tuple(line.split(','))
-        x.append(f1)
-        y.append(f2)
-        _class.append(cls)
+        f1, f2, _cls = tuple(line.split(','))
+        _x.append(f1)
+        _y.append(f2)
+        _class.append(value[_cls])
 
-    return x, y, _class
+    return _x, _y, _class
 
 
 # load the training set
 x, y, cls = load("training_set.data")
 
-print(x, y, cls)
+class_A = [i for i in range(len(cls)) if cls[i] == value[A]]
+class_B = [i for i in range(len(cls)) if cls[i] == value[B]]
+
+print(class_A)
+
+# plot class A data with red color
+plt.plot([x[i] for i in class_A], [y[i] for i in class_A], color[A] + 'o')
+
+# plot class B data with red color
+plt.plot([x[i] for i in class_B], [y[i] for i in class_B], color[B] + 'o')
+
+plt.show()
