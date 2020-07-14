66a1fa828cdf36b6a8d8b9c6c026c2760db08f85
Ali Parvizi <snparvizi75@gmail.com>
2020-07-14 12:43:57 +0430

    binary classification

--- partC.py
+++ partC.py
@@ -29,8 +29,8 @@ def load(file):
 
     for line in data:
         f1, f2, _cls = tuple(line.split(','))
-        _x.append(f1)
-        _y.append(f2)
+        _x.append(float(f1))
+        _y.append(float(f2))
         _class.append(value[_cls])
 
     return _x, _y, _class
@@ -40,20 +40,81 @@ def sigmoid(z):
     return 1 / (1 + np.exp(-z))
 
 
-def cost()
+def predict(_features, _weights):
+    z = np.dot(_features, _weights)
+    d = sigmoid(z)
+    return d
+
+
+def cost_function(_features, _labels, _weights):
+    observations = len(_labels)
+    predictions = predict(_features, _weights)
+    class1_cost = _labels * np.log(predictions)
+    class2_cost = (1 - _labels) * np.log(1 - predictions)
+    cost = class1_cost + class2_cost
+    cost = cost.sum() / observations
+
+    return cost
+
+
+def update_weights(_features, _labels, _weights, lr):
+    N = len(_features)
+    predictions = predict(_features, _weights)
+
+    gradient = np.dot(_features.T, predictions - _labels) / N
+    _weights -= gradient * lr
+
+    return _weights
+
+
+def decision_boundary(prob):
+    return 1 if prob >= .5 else 0
+
+
+def classify(predictions):
+    """
+    input  - N element array of predictions between 0 and 1
+    output - N element array of 0s (False) and 1s (True)
+    """
+    return np.array([decision_boundary(prb) for prb in predictions])
+
+
+def train(_features, _labels, _weights, lr, iters):
+    cost_history = []
+
+    for i in range(iters):
+        _weights = update_weights(_features, _labels, _weights, lr)
+
+        # Calculate error for auditing purposes
+        cost = cost_function(_features, _labels, _weights)
+        cost_history.append(cost)
+
+        # Log Progress
+        if i % 10000 == 0:
+            print("iter: " + str(i) + " cost: " + str(cost))
+
+    return _weights, cost_history
+
 
 # load the training set
-x, y, cls = load("training_set.data")
+x, y, labels = load("training_set.data")
 
 # class A indexes
-class_A = [i for i in range(len(cls)) if cls[i] == value[A]]
+class_A = [i for i in range(len(labels)) if labels[i] == value[A]]
 # class B indexes
-class_B = [i for i in range(len(cls)) if cls[i] == value[B]]
+class_B = [i for i in range(len(labels)) if labels[i] == value[B]]
+
+features = np.array([[1, x[i], y[i]] for i in range(len(labels))])
+labels = np.array(labels)
+
+initial_weights = np.zeros(3)
+
+weights, history = train(features, labels, initial_weights, 0.1, 1000)
 
 # plot class A data with red color
-plt.plot([x[i] for i in class_A], [y[i] for i in class_A], color[A] + 'o', markersize=3)
+plt.plot([x[i] for i in class_A], [y[i] for i in class_A], color[A] + 'o', markersize=3, label=A)
 # plot class B data with red color
-plt.plot([x[i] for i in class_B], [y[i] for i in class_B], color[B] + 'o', markersize=3)
+plt.plot([x[i] for i in class_B], [y[i] for i in class_B], color[B] + 'o', markersize=3, label=B)
 
 plt.legend(loc='best')
 plt.show()
